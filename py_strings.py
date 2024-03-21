70c8d9a970e7d070a26ed670fa17c940443ce49e
Saillphrogg <161702084+Saillphrogg@users.noreply.github.com>
2024-03-21 11:18:16 +0100

    Add files via upload

--- py_strings.py
+++ py_strings.py
@@ -1,83 +1,33 @@
 def reverse(text: str) -> str:
-    """
-    Return the 'text' backwards.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    str
-        The text written backwards.
-    """
+    return(text[::-1])
     pass
-
 
 def first_to_upper(text: str) -> str:
-    """
-    Changes each first character of the word to uppercase.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    str
-        The modified text
-    """
+    return ' '.join(word[0].capitalize if word[0].islower() else word for word in text.split())
     pass
 
-
 def count_vowels(text: str) -> int:
-    """
-    Counts number of vovels in the text.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    inp
-        Number of vowels.
-    """
+    samo="aeiouAEIOU"
+    ile=0
+    for c in text:
+        if c in samo:
+            ile += 1
+    return ile
     pass
 
 
 def sum_digits(text: str) -> int:
-    """
-    Finds all digitts in the text and returns its sum.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    int
-        Sum of all digits in the text.
-    """
+    return sum(int(c) for c in text if c.isdigit())
     pass
 
 
 def search_substr(text: str, sub: str) -> int:
-    """
-    Search for sub(string) in the text. Returns the position or None.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    int or None
-        Position of the sub(string) or None.
-    """
+    for i in range(len(text)-len(sub)+1):
+    	if text[i:i+len(sub)]==sub:
+    		return i
+    return None
     pass
+
+
+
+
