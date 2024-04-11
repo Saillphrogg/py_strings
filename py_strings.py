ebd13a3f2914b99695f90556f7ac6eabc7d04fca
Saillphrogg <161702084+Saillphrogg@users.noreply.github.com>
2024-04-11 11:01:56 +0200

    update 0.3
    test adjustements, redo of first to upper

--- py_strings.py
+++ py_strings.py
@@ -1,33 +1,47 @@
 def reverse(text: str) -> str:
-    return(text[::-1])
-    pass
+    """
+    reverse the text and return it
+    """
+    return text[::-1]
 
 def first_to_upper(text: str) -> str:
-    return ' '.join([word[0].capitalize() if word[0].islower() else word for word in text.split()])
-    pass
+    """
+    split the text and if the first leter is lower then capitalize it
+    """
+    return ' '.join([word[0].upper()+word[1:] for word in text.split()])
 
 def count_vowels(text: str) -> int:
-    samo="aeiouAEIOU"
+    """
+    create vowels string
+    crete an intiger counting value
+    for char in text
+        if char in vowels:
+            counting+=1
+    return counting
+    """
+    samo="aeiouyąęóAEIOUYĄĘÓ"
     ile=0
     for c in text:
         if c in samo:
             ile += 1
     return ile
-    pass
 
 
 def sum_digits(text: str) -> int:
+    """
+    return the sum of c ion text if c is a digit
+    """
     return sum(int(c) for c in text if c.isdigit())
-    pass
 
 
 def search_substr(text: str, sub: str) -> int:
+    """
+    for i in range lenght of text - lenght of substring + 1
+        if the section of the string starting from i to lenght of substring == substring
+            return i
+    else return None
+    """
     for i in range(len(text)-len(sub)+1):
-    	if text[i:i+len(sub)]==sub:
-    		return i
+        if text[i:i+len(sub)]==sub:
+            return i
     return None
-    pass
-
-
-
-
